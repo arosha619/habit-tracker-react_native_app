51e33ac726f18dcda230ec53e01bb25b3c7c9d29
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2025-07-27 12:19:28 +0530

    layout fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,42 @@
-import { Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const isAuth = false;
-  const [isReady, setIsReady] = useState(false);
+  const { user, isLoadingUser } = useAuth();
+  const segments = useSegments();
 
   useEffect(() => {
-    if (!isAuth && isReady) {
-      router.replace("/auth");
-    }
-    setIsReady(true);
-  }, [isAuth, isReady, router]);
+    const inAuthGroup = segments[0] === "auth";
 
-  // Don't render children until ready to prevent early navigation
-  if (!isReady) {
-    return null;
-  }
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
+    }
+  }, [user, segments]);
 
   return <>{children}</>;
 }
 
 export default function RootLayout() {
   return (
-    <RouteGuard>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </RouteGuard>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <PaperProvider>
+          <SafeAreaProvider>
+            <RouteGuard>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </RouteGuard>
+          </SafeAreaProvider>
+        </PaperProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
